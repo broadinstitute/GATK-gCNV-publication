489e95c73ede67cb63b624d8f0a981eba26c1853
Andrey Smirnov <asmirnov2009@gmail.com>
2020-05-27 17:06:22 -0400

    Added per bin evaluation logic

--- evaluation_code/pygcnveval/pygcnveval/evaluation_result.py
+++ evaluation_code/pygcnveval/pygcnveval/evaluation_result.py
@@ -1,5 +1,7 @@
+import numpy as np
 
-class EvaluationResult:
+
+class PerEventEvaluationResult:
     DEFAULT_EVENT_SIZE_BINS = list(range(25))
 
     def __init__(self, event_size_bins=DEFAULT_EVENT_SIZE_BINS):
@@ -10,22 +12,58 @@ class EvaluationResult:
         self.recall_size_to_tp = {i: 0 for i in event_size_bins}
         self.recall_size_to_fn = {i: 0 for i in event_size_bins}
 
-    def update_precision(self, num_bins: int, event_valid: bool):
+    def update_precision(self, num_intervals: int, event_valid: bool):
+        # TODO implement the general case of binning
+        index = min(num_intervals - 1, len(self.event_size_bins) - 1)
         if event_valid:
-            self.precision_size_to_tp[num_bins - 1] += 1
+            self.precision_size_to_tp[index] += 1
         else:
-            self.precision_size_to_fp[num_bins - 1] += 1
+            self.precision_size_to_fp[index] += 1
 
-    def update_recall(self, num_bins: int, event_valid: bool):
+    def update_recall(self, num_intervals: int, event_valid: bool):
+        # TODO implement the general case of binning
+        index = min(num_intervals - 1, len(self.event_size_bins) - 1)
         if event_valid:
-            self.recall_size_to_tp[num_bins - 1] += 1
+            self.recall_size_to_tp[index] += 1
         else:
-            self.recall_size_to_fn[num_bins - 1] += 1
+            self.recall_size_to_fn[index] += 1
 
     def __eq__(self, other):
-        if isinstance(other, EvaluationResult):
-            return self.precision_size_to_tp == other.precision_size_to_tp\
-                   and self.precision_size_to_fp == other.precision_size_to_fp\
-                   and self.recall_size_to_tp == other.recall_size_to_tp\
+        if isinstance(other, PerEventEvaluationResult):
+            return self.precision_size_to_tp == other.precision_size_to_tp \
+                   and self.precision_size_to_fp == other.precision_size_to_fp \
+                   and self.recall_size_to_tp == other.recall_size_to_tp \
                    and self.recall_size_to_fn == other.recall_size_to_fn
+        return False
+
+
+class PerBinEvaluationResult:
+
+    def __init__(self, quality_matrix: np.ndarray, number_points):
+        self.quality_thresholds = sorted(list(set(np.percentile(
+            quality_matrix, [(i / number_points) * 100 for i in range(number_points + 1)]))))
+        self.true_positives = {q: 0 for q in self.quality_thresholds}
+        self.false_positives = {q: 0 for q in self.quality_thresholds}
+        self.false_negatives = {q: 0 for q in self.quality_thresholds}
+
+    def set_values_for_quality_cutoff(self, threshold, true_positives, false_positives, false_negatives):
+        self.true_positives[threshold] = true_positives
+        self.false_positives[threshold] = false_positives
+        self.false_negatives[threshold] = false_negatives
+
+    def get_precision_array(self):
+        return np.array([self.true_positives[q] / (self.true_positives[q] + self.false_positives[q])
+                         if self.true_positives[q] != 0. else 0
+                         for q in self.quality_thresholds])
+
+    def get_recall_array(self):
+        return np.array([self.true_positives[q] / (self.true_positives[q] + self.false_negatives[q])
+                         if self.true_positives[q] != 0. else 0
+                         for q in self.quality_thresholds])
+
+    def __eq__(self, other):
+        if isinstance(other, PerBinEvaluationResult):
+            return self.true_positives == other.true_positives \
+                   and self.false_positives == other.false_positives \
+                   and self.false_negatives == other.false_negatives
         return False
--- evaluation_code/pygcnveval/tests/callset__test.py
+++ evaluation_code/pygcnveval/tests/callset__test.py
@@ -1,8 +1,9 @@
-from callset import GCNVCallset, TruthCallset
+from callset import CallsetMatrixView, Callset, GCNVCallset, TruthCallset
 from interval_collection import IntervalCollection
 from event import EventType
 import pandas as pd
 import pyranges as pr
+import numpy as np
 
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 
@@ -16,20 +17,42 @@ GCNV_CALLSET_TEST_VALUES = [('1', 1001, 3000, EventType.DEL, 2, 60),
                             ('2', 4001, 5000, EventType.DUP, 1, 100),
                             ('2', 6001, 7000, EventType.REF, 1, 20)]
 GCNV_CALLSET_SAMPLE_NAME = "SAMPLE_1"
-GCNV_CALLSET_TEST_DF = pd.DataFrame(GCNV_CALLSET_TEST_VALUES, columns=GCNVCallset.vcf_columns)
-GCNV_CALLSET_TEST_DF = GCNV_CALLSET_TEST_DF.astype(GCNVCallset.vcf_column_types)
+GCNV_CALLSET_TEST_DF = pd.DataFrame(GCNV_CALLSET_TEST_VALUES, columns=Callset.CALLSET_COLUMNS)
+GCNV_CALLSET_TEST_DF = GCNV_CALLSET_TEST_DF.astype(Callset.CALLSET_COLUMN_TYPES)
 GCNV_CALLSET_TEST_PYRANGE_EXPECTED = pr.PyRanges(GCNV_CALLSET_TEST_DF)
+
 
 # Truth test callset resources
 TRUTH_CALLSET_TEST_BED = "./test_files/truth.bed"
-TRUTH_CALLSET_VALUES = [('1', 501, 4500, 'DEL_chr1_1', EventType.DEL, frozenset(['SAMPLE_0', 'SAMPLE_1', 'SAMPLE_2']), 1.0, 3),
-                        ('1', 7001, 10000, 'DEL_chr1_2', EventType.DEL, frozenset(['SAMPLE_0']), 1./3, 2),
-                        ('2', 1001, 3000, 'DUP_chr2_1', EventType.DUP, frozenset(['SAMPLE_0']), 1./3, 0),
-                        ('2', 4001, 7000, 'DUP_chr2_2', EventType.DUP, frozenset(['SAMPLE_0', 'SAMPLE_1']), 2./3, 2)]
-TRUTH_CALLSET_TEST_DF = pd.DataFrame(TRUTH_CALLSET_VALUES, columns=TruthCallset.bed_columns)
-TRUTH_CALLSET_TEST_DF = TRUTH_CALLSET_TEST_DF.astype(TruthCallset.bed_column_types)
+TRUTH_CALLSET_VALUES = [('1', 501, 4500, 'DEL_chr1_1', EventType.DEL, frozenset(['SAMPLE_0', 'SAMPLE_1', 'SAMPLE_2']), 0.75, 3),
+                        ('1', 7001, 10000, 'DEL_chr1_2', EventType.DEL, frozenset(['SAMPLE_0']), 1./2, 2),
+                        ('2', 1001, 3000, 'DUP_chr2_1', EventType.DUP, frozenset(['SAMPLE_0']), 1./2, 0),
+                        ('2', 4001, 7000, 'DUP_chr2_2', EventType.DUP, frozenset(['SAMPLE_0', 'SAMPLE_1']), 1./2, 2)]
+SAMPLES_TO_KEEP = {'SAMPLE_0', 'SAMPLE_1', 'SAMPLE_2'}
+TRUTH_CALLSET_TEST_DF = pd.DataFrame(TRUTH_CALLSET_VALUES, columns=TruthCallset.JOINT_CALLSET_COLUMNS)
+TRUTH_CALLSET_TEST_DF = TRUTH_CALLSET_TEST_DF.astype(TruthCallset.JOINT_CALLSET_COLUMN_TYPES)
 TRUTH_CALLSET_TEST_PYRANGE_EXPECTED = pr.PyRanges(TRUTH_CALLSET_TEST_DF)
 TRUTH_CALLSET_TEST_FILTERED_PYRANGE_EXPECTED = pr.PyRanges(TRUTH_CALLSET_TEST_DF.iloc[[0, 1, 3]])
+
+TRUTH_CALLSET_SAMPLE_TO_PYRANGE_MAP = {'SAMPLE_0': [('1', 501, 4500, EventType.DEL, 3, 0),
+                                                    ('1', 7001, 10000, EventType.DEL, 2, 0),
+                                                    ('2', 4001, 7000, EventType.DUP, 2, 0)],
+                                       'SAMPLE_1': [('1', 501, 4500, EventType.DEL, 3, 0),
+                                                    ('2', 4001, 7000, EventType.DUP, 2, 0)],
+                                       'SAMPLE_2': [('1', 501, 4500, EventType.DEL, 3, 0)]}
+for s in TRUTH_CALLSET_SAMPLE_TO_PYRANGE_MAP.keys():
+    TRUTH_CALLSET_SAMPLE_TO_PYRANGE_MAP[s] = pr.PyRanges(pd.DataFrame(TRUTH_CALLSET_SAMPLE_TO_PYRANGE_MAP[s],
+                                                                      columns=Callset.CALLSET_COLUMNS)
+                                                         .astype(Callset.CALLSET_COLUMN_TYPES))
+
+TRUTH_CALLSET_CALLSET_MATRIX_VIEW_SAMPLES = ['SAMPLE_0', 'SAMPLE_1', 'SAMPLE_2']
+TRUTH_CALLSET_CALLSET_MATRIX_VIEW_GENOTYPES = np.array([[1, 1, 1, 0, 1, 1, 2, 2],
+                                                        [1, 1, 1, 0, 0, 0, 2, 2],
+                                                        [1, 1, 1, 0, 0, 0, 0, 0]])
+TRUTH_CALLSET_CALLSET_MATRIX_VIEW_QUALITIES = np.zeros(shape=TRUTH_CALLSET_CALLSET_MATRIX_VIEW_GENOTYPES.shape)
+TRUTH_CALLSET_CALLSET_MATRIX_VIEW_EXPECTED = CallsetMatrixView(TRUTH_CALLSET_CALLSET_MATRIX_VIEW_SAMPLES,
+                                                               TRUTH_CALLSET_CALLSET_MATRIX_VIEW_GENOTYPES,
+                                                               TRUTH_CALLSET_CALLSET_MATRIX_VIEW_QUALITIES)
 
 
 def test_gcnv_callset():
@@ -40,18 +63,33 @@ def test_gcnv_callset():
 def test_truth_callset():
     interval_collection = IntervalCollection.read_interval_list(ANALYZED_INTERVALS)
     # Test callset parsing
-    truth_callset_actual = TruthCallset.read_in_callset(truth_callset_bed=TRUTH_CALLSET_TEST_BED,
-                                                        interval_collection=interval_collection)
+    truth_callset_actual = TruthCallset.read_in_callset(truth_callset_bed_file=TRUTH_CALLSET_TEST_BED,
+                                                        interval_collection=interval_collection,
+                                                        samples_to_keep=SAMPLES_TO_KEEP)
     assert truth_callset_actual.truth_callset_pyrange.df.equals(TRUTH_CALLSET_TEST_PYRANGE_EXPECTED.df)
     # Test filtering
 
-    truth_callset_actual.filter_out_uncovered_events(interval_collection, overall_reciprocal_overlap=0.3, padding=0)
+    truth_callset_actual.filter_out_uncovered_events(interval_collection, overall_reciprocal_overlap=0.3)
     assert truth_callset_actual.truth_callset_pyrange.df.equals(TRUTH_CALLSET_TEST_FILTERED_PYRANGE_EXPECTED.df)
+
+    for s in truth_callset_actual.sample_set:
+        assert truth_callset_actual.sample_to_pyrange_map[s].df.equals(TRUTH_CALLSET_SAMPLE_TO_PYRANGE_MAP[s].df)
+
+
+def test_sample_by_interval_matrix():
+    interval_collection = IntervalCollection.read_interval_list(ANALYZED_INTERVALS)
+    # Test callset parsing
+    truth_callset = TruthCallset.read_in_callset(truth_callset_bed_file=TRUTH_CALLSET_TEST_BED,
+                                                 interval_collection=interval_collection,
+                                                 samples_to_keep=SAMPLES_TO_KEEP)
+    callset_matrix_view_actual = truth_callset.get_callset_matrix_view(interval_collection)
+    assert callset_matrix_view_actual == TRUTH_CALLSET_CALLSET_MATRIX_VIEW_EXPECTED
 
 
 def main():
     test_gcnv_callset()
     test_truth_callset()
+    test_sample_by_interval_matrix()
 
 
 if __name__ == '__main__':
